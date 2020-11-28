73fe787003865a2de49cd8c12a7155256209489c
KiHooooon <hksy113@gmail.com>
2020-11-29 01:24:35 +0900

    MongoDB 모듈 추가 및 DB 연결

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://KIHOON:!gksrlgns113@kh.iemrg.mongodb.net/KH?retryWrites=true&w=majority',{
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => {
+    console.log('MongoDB Connected...');
+}).catch(err => {
+    console.log(err);
+})
+
 app.get('/', (req, res) => {
     res.send('Hi');
 })
