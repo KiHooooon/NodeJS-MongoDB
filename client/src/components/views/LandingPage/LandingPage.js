6c5a99f6501f3d222063fc22dfb682c68242e743
KiHooooon <hksy113@gmail.com>
2020-11-30 23:44:01 +0900

    로그아웃 기능 구현

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -2,15 +2,30 @@ import React, {useEffect} from 'react';
 import axios from 'axios';
 import NavBar from '../NavBar/NavBar';
 
-function LandingPage() {
+function LandingPage(props) {
     useEffect(() => {
         axios.get('/api/hello')
         .then(response => console.log(response.data))
     }, [])
+
+    const onClickhandler = () => {
+        axios.get('/api/users/logout')
+            .then(response => {
+                console.log(response.data)
+                if(response.data.success) {
+                    props.history.push('/login');
+                } else {
+                    alert('로그아웃 실패');
+                }
+            })
+    }
     return (
         <div>
+            <button onClick={onClickhandler}>
+                로그아웃
+            </button>
             <NavBar />
-            LandingPage
+            
         </div>
     )
 }
